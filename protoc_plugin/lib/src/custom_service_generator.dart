365ce12a6de5bd9652b6c529f0079d27d90f7c61
zhilei-ola <chen.zhilei@olaola.chat>
2022-01-04 23:53:54 +0800

    update api template

--- protoc_plugin/lib/src/custom_service_generator.dart
+++ protoc_plugin/lib/src/custom_service_generator.dart
@@ -132,14 +132,15 @@ class _CustomApiMethod {
 
   void generateClientStub(IndentingWriter out) {
     out.println();
-    out.addBlock('$_clientReturnType $_dartName($_argumentType request) async {', '}', () {
+    out.addBlock('static $_clientReturnType $_dartName($_argumentType request) async {', '}', () {
     out.println("String url = '\${System.domain}$_apiPrefix$_dartName';");
 
     out.println("XhrResponse response = await Xhr.postPb(url, request.toRequestBody(),headers: {'Accept': 'application/protobuf','Content-Type': 'application/protobuf'},throwOnError: false);");
     out.println("if (response.error == null) {");
     out.println("return $_responseType.fromBuffer(response.bodyBytes);");
     out.println("}else if(response.error?.code == XhrErrorCode.HttpStatus){");
-    out.println("  // TODO: parse http code ");
+    out.println("  // TODO: parse http code inline ");
+    out.println("  throw response.error;");
     out.println("}");
     out.println("return null;");
     });
